318ac965473e5de7d4b60a4441d0a1cabb3a23b7
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-09-02 09:52:18 -0300

    feat: :sparkles: Criando header do app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList } from 'react-native';
 
 export default class App extends Component {
   constructor(props) {
@@ -8,15 +8,41 @@ export default class App extends Component {
   }
   render() {
     return (
-      <View style={style.container}>
-        <Text> Init App React</Text>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Image source={require('./src/img/logo.png')}  style={styles.logo} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require('./src/img/send.png')} style={styles.send} />
+          </TouchableOpacity>
+        </View>
+        <FlatList showsHorizontalScrollIndicator={false} data={this.state.feed} renderItem={({item}) => <Lista data={item} />}/>
       </View >
     );
   }
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  header: {
+    height: 55,
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 5,
+    justifyContent: 'space-between',
+    borderBottomWidth: 0.2,
+    shadowColor: '#000',
+    elevation: 1,
+  },
+  logo: {
+
+  },
+  send: {
+    width: 23,
+    height: 23,
   },
 });
