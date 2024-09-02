9100b3244696154d8296460381f54a0e00b259d2
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-09-02 09:31:23 -0300

    chore: :memo: Subindo arquivos imagens necessário

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default class App extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
       <View style={style.container}>
